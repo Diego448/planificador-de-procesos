633ee1fa568878c6642fb49f925a0c93a171f5ff
Diego Serrano Gómez <diegosg448@gmail.com>
2017-03-09 00:17:56 -0600

    Add files via upload

---
+++ src/planificador/de/procesos/PlanificadorDeProcesos.java
@@ -0,0 +1,16 @@
+package planificador.de.procesos;
+
+/**
+ *
+ * @author Diego Serrano Gómez
+ *         Gerardo Ruíz Santana
+ */
+public class PlanificadorDeProcesos {
+    //Declaración e inicialización de ventana menuPrincipal
+    static MenuPrincipal menuPrincipal = new MenuPrincipal();
+    public static void main(String[] args) {
+        //Mostrar ventana menuPrincipal
+        menuPrincipal.setVisible(true);
+    }
+    
+}
---
+++ src/planificador/de/procesos/Proceso.java
@@ -0,0 +1,17 @@
+package planificador.de.procesos;
+
+/**
+ *
+ * @author Diego
+ */
+public class Proceso {
+    String nombre;
+    int duracion;
+    char prioridad;
+	
+    public Proceso() {
+	this.nombre = "";
+	this.duracion = 0;
+    }
+
+}
